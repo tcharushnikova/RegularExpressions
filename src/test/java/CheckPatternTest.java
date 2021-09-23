d5a9218ce5987b6245600c49d43585c39455c492
tcharushnikova <57858847+tcharushnikova@users.noreply.github.com>
2021-09-23 10:20:14 +0300

    Добавлены тесты для проверки GUID, URL и пароля

--- src/test/java/CheckPatternTest.java
+++ src/test/java/CheckPatternTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 public class CheckPatternTest {
+    //<editor-fold desc="Проверка IP">
     @org.junit.Test
     public void correctIPTest1() {
         assertTrue(CheckPattern.isIP("127.0.0.1"));
@@ -20,4 +21,61 @@ public class CheckPatternTest {
     public void wrongIPTest2() {
         assertFalse(CheckPattern.isIP("abc.def.gha.bcd"));
     }
+    //</editor-fold>
+
+    //<editor-fold desc="Проверка GUID">
+    @org.junit.Test
+    public void correctGUIDTest() {
+        assertTrue(CheckPattern.isGUID("e02fd0e4-00fd-090A-ca30-0d00a0038ba0"));
+    }
+
+    @org.junit.Test
+    public void wrongGUIDTest() {
+        assertFalse(CheckPattern.isGUID("e02fd0e400fd090Aca300d00a0038ba0"));
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Проверка URL">
+    @org.junit.Test
+    public void correctURLTest1() {
+        assertTrue(CheckPattern.isURL("http://www.example.com"));
+    }
+
+    @org.junit.Test
+    public void correctURLTest2() {
+        assertTrue(CheckPattern.isURL("http://example.com"));
+    }
+
+    @org.junit.Test
+    public void wrongURLTest1() {
+        assertFalse(CheckPattern.isURL("Just Text"));
+    }
+
+    @org.junit.Test
+    public void wrongURLTest2() {
+        assertFalse(CheckPattern.isURL("http://a.com"));
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Проверка пароля">
+    @org.junit.Test
+    public void correctPasswordTest1() {
+        assertTrue(CheckPattern.isPassword("C00l_Pass"));
+    }
+
+    @org.junit.Test
+    public void correctPasswordTest2() {
+        assertTrue(CheckPattern.isPassword("SupperPas1"));
+    }
+
+    @org.junit.Test
+    public void wrongPasswordTest1() {
+        assertFalse(CheckPattern.isPassword("Cool_pass"));
+    }
+
+    @org.junit.Test
+    public void wrongPasswordTest2() {
+        assertFalse(CheckPattern.isPassword("C00l"));
+    }
+    //</editor-fold>
 }
