0abc5150c5a625d27aafa95eb0343e85a4cecbf7
tcharushnikova <57858847+tcharushnikova@users.noreply.github.com>
2021-09-24 15:09:34 +0300

    Добавлено несколько дополнительных тестов

--- src/test/java/CheckPatternTest.java
+++ src/test/java/CheckPatternTest.java
@@ -13,6 +13,11 @@ public class CheckPatternTest {
     }
 
     @org.junit.Test
+    public void correctIPTest3() {
+        assertTrue(CheckPattern.isIP("192.168.0.1"));
+    }
+
+    @org.junit.Test
     public void wrongIPTest1() {
         assertFalse(CheckPattern.isIP("1300.6.7.8"));
     }
@@ -21,17 +26,32 @@ public class CheckPatternTest {
     public void wrongIPTest2() {
         assertFalse(CheckPattern.isIP("abc.def.gha.bcd"));
     }
+
+    @org.junit.Test
+    public void wrongIPTest3() {
+        assertFalse(CheckPattern.isIP("254.hzf.bar.10"));
+    }
     //</editor-fold>
 
     //<editor-fold desc="Проверка GUID">
     @org.junit.Test
-    public void correctGUIDTest() {
+    public void correctGUIDTest1() {
+        assertTrue(CheckPattern.isGUID("{e02fa0e4-01ad-090A-c130-0d05a0008ba0}"));
+    }
+
+    @org.junit.Test
+    public void correctGUIDTest2() {
         assertTrue(CheckPattern.isGUID("e02fd0e4-00fd-090A-ca30-0d00a0038ba0"));
     }
 
     @org.junit.Test
-    public void wrongGUIDTest() {
+    public void wrongGUIDTest1() {
         assertFalse(CheckPattern.isGUID("e02fd0e400fd090Aca300d00a0038ba0"));
+    }
+
+    @org.junit.Test
+    public void wrongGUIDTest2() {
+        assertFalse(CheckPattern.isGUID("02fa0e4-01ad-090A-c130-0d05a0008ba0}"));
     }
     //</editor-fold>
 
@@ -47,6 +67,21 @@ public class CheckPatternTest {
     }
 
     @org.junit.Test
+    public void correctURLTest3() {
+        assertTrue(CheckPattern.isURL("https://example.ru"));
+    }
+
+    @org.junit.Test
+    public void correctURLTest4() {
+        assertTrue(CheckPattern.isURL("https://sub.example-ru.com:8080"));
+    }
+
+    @org.junit.Test
+    public void correctURLTest5() {
+        assertTrue(CheckPattern.isURL("ex.com/index.html#bookmark"));
+    }
+
+    @org.junit.Test
     public void wrongURLTest1() {
         assertFalse(CheckPattern.isURL("Just Text"));
     }
@@ -54,6 +89,11 @@ public class CheckPatternTest {
     @org.junit.Test
     public void wrongURLTest2() {
         assertFalse(CheckPattern.isURL("http://a.com"));
+    }
+
+    @org.junit.Test
+    public void wrongURLTest3() {
+        assertFalse(CheckPattern.isURL("http://www.domain-.com"));
     }
     //</editor-fold>
 
